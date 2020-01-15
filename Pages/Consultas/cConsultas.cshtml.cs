9a7fc1aed6296d125ee4448eee79bf3d7fa10ea0
Erick Josue Gutierrez Hernandez <erick_gutierrez@ucne.edu.do>
2020-01-15 00:00:19 -0300

    Comenzando la consulta

---
+++ Pages/Consultas/cConsultas.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tarea1Aplicada2
+{
+    public class cConsultasModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
